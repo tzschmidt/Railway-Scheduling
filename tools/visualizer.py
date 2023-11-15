bc028a279677bf4261162c6ba3b020cb3fda1d91
tzschmidt <tzschmidt@gmx.de>
2023-11-15 12:06:25 +0100

    Start work on visualizer

---
+++ tools/visualizer.py
@@ -0,0 +1,92 @@
+
+import sys
+import re
+
+from flatland.envs.rail_env import RailEnv
+from flatland.utils.rendertools import RenderTool
+
+
+# TODO implement
+def get_env(file):
+    return
+
+
+def get_actions(file, n_agents):
+    
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    f.close()
+
+    # last solution
+    for l in lines:
+        if l[0] == "a":
+            solution = l
+            
+    # actions
+    r1 = re.compile(r'\(\d+,\d+,\d+\)')
+    m1 = r1.findall(solution)
+
+    r2 = re.compile(r'\((\d+),(\d+),(\d+)\)')
+    actions = [[0] * (len(m1)//n_agents)] * n_agents
+    for i in m1:
+        m2 = r2.match(i)
+        actions[int(m2.group(1))][int(m2.group(3))] = int(m2.group(2))
+
+    return actions
+
+# TDOD finish  
+def visualize(env, actions):
+    env_renderer = RenderTool(env,
+                          #agent_render_variant=AgentRenderVariant.ONE_STEP_BEHIND,
+                          show_debug=False,
+                          screen_height=600,  # Adjust these parameters to fit your resolution
+                          screen_width=800)  # Adjust these parameters to fit your resolution
+    
+    action_dict = dict()
+
+    # exception -> problem in env
+    env.step(action_dict)
+    
+    # arbitrary number of steps
+    for step in range(50):
+        # action for each agent in the environment
+        for a in range(env.get_num_agents()):
+            action_dict.update({a: actions[a][step]})
+
+        # make step
+        next_obs, all_rewards, done, _ = env.step(action_dict)
+
+        if env_renderer is not None:
+            env_renderer.render_env(show=True, show_observations=False, show_predictions=False)
+            env_renderer.gl.save_image('tmp/frames/flatland_frame_{:04d}.png'.format(step))
+
+        print("\n Their current statuses are:")
+        print("============================")
+        for agent_idx, agent in enumerate(env.agents):
+            print("Agent {} status is: {} with its current position being {}".format(
+                agent_idx, str(agent.state), str(agent.position)))
+            
+        if done['__all__']:
+            break
+        
+    # close the renderer / rendering window
+    if env_renderer is not None:
+        env_renderer.close_window()
+    return
+
+def main(argv):
+    # TODO check input
+    env_file = argv[1]
+    asp_file = argv[2]
+    
+    # env = get_env(env_file)
+    
+    # actions = get_actions(asp_file, env.get_num_agents())
+    
+    # visualize(env, actions)
+   
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main(sys.argv)
+    else:
+        print("Usage: visualizer.py <env-file.json> <asp-out.txt>")
